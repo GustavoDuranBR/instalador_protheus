c80615b206106077596e580c5593a5163b7f8fea
Luiz Gustavo Duran Leite_ <luiz.gleite@totvs.com.br>
2024-08-21 10:34:23 -0300

    Correções e melhorias

--- funcoes.py
+++ funcoes.py
@@ -3,12 +3,13 @@ import requests
 import tkinter as tk
 
 def create_folder_structure(version, log_box):
-    base_directory = f"C:\\TOTVS\\{version}"
+    base_directory = f"C:\\TOTVS"
     directories = [
-        os.path.join(base_directory, "Protheus"),
-        os.path.join(base_directory, "Protheus", "Apo"),
+        os.path.join(base_directory, f"{version}", "Apo"),
         os.path.join(base_directory, "Protheus", "bin"),
-        os.path.join(base_directory, "Protheus_Data"),
+        os.path.join(base_directory, "Protheus", "bin", "Appserver"),
+        os.path.join(base_directory, "Protheus", "bin", "SmartClient"),
+        os.path.join(base_directory, f"{version}","Protheus_Data"),
         os.path.join(base_directory, "TotvsDBAccess")
     ]
     for directory in directories:
@@ -20,7 +21,7 @@ def create_folder_structure(version, log_box):
 
 def get_download_url(build, appserver):
     base_url_appserver = "https://arte.engpro.totvs.com.br/tec/appserver/"
-    base_url_smartclient = "https://arte.engpro.totvs.com.br/tec/smartclient"
+    base_url_smartclient = "https://arte.engpro.totvs.com.br/tec/smartclient/"
     base_url_dbaccess = "https://arte.engpro.totvs.com.br/tec/dbaccess/windows/64/latest/dbaccess.zip"
     base_url_smartclientwebapp = "https://arte.engpro.totvs.com.br/tec/smartclientwebapp/"
 
@@ -45,25 +46,51 @@ def get_download_url(build, appserver):
     return urls
 
 def download_files(version, urls, log_box):
-    base_directory = f"C:\\TOTVS\\{version}"
+    base_directory = f"C:\\TOTVS\\Download\\{version}"
+
+    if not os.path.exists(base_directory):
+        os.makedirs(base_directory)
+
     for url in urls:
         file_name = os.path.join(base_directory, url.split("/")[-1])
-        log_box.insert(tk.END, f"Baixando {url}...\n")
+        log_box.insert(tk.END, f"Iniciando download de {url.split('/')[-1]}...\n")
         log_box.see(tk.END)
+        
         response = requests.get(url)
         with open(file_name, 'wb') as file:
             file.write(response.content)
+        
         log_box.insert(tk.END, f"Download de {file_name} concluído!\n")
         log_box.see(tk.END)
 
 def download_protheus(version, appserver, build, log_box):
-    log_box.insert(tk.END, "Iniciando o download...\n")
+    log_box.insert(tk.END, "Iniciando o processo de download...\n")
     log_box.see(tk.END)
     
+    # Obtém as URLs para download
     download_urls = get_download_url(build, appserver)
+    
+    # Cria a estrutura de pastas
     create_folder_structure(version, log_box)
-    download_files(version, download_urls, log_box)
-    log_box.insert(tk.END, "Download concluído.\n")
+    
+    # Realiza os downloads um por vez
+    log_box.insert(tk.END, "Baixando AppServer...\n")
+    log_box.see(tk.END)
+    download_files(version, [download_urls[0]], log_box)  # AppServer
+    
+    log_box.insert(tk.END, "Baixando SmartClient...\n")
+    log_box.see(tk.END)
+    download_files(version, [download_urls[1]], log_box)  # SmartClient
+    
+    log_box.insert(tk.END, "Baixando DBAccess...\n")
+    log_box.see(tk.END)
+    download_files(version, [download_urls[2]], log_box)  # DBAccess
+    
+    log_box.insert(tk.END, "Baixando SmartClient WebApp...\n")
+    log_box.see(tk.END)
+    download_files(version, [download_urls[3]], log_box)  # SmartClient WebApp
+    
+    log_box.insert(tk.END, "Todos os downloads foram concluídos com sucesso.\n")
     log_box.see(tk.END)
 
 def download_base_congelada(version, log_box):
--- gerar_appserver_ini.py
+++ gerar_appserver_ini.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import tkinter as tk
 from tkinter import ttk  # Adicionar essa linha
+from funcoes import create_folder_structure
 
 def copiar_appserver_ini(version, log_box):
     # Diretório de origem com base na versão selecionada
@@ -18,7 +19,7 @@ def copiar_appserver_ini(version, log_box):
     ini_file = os.path.join(source_dir, "appserver.ini")
 
     # Caminho de destino
-    dest_dir = os.path.join(f"C:\\TOTVS\\{version}", "Protheus", "bin", "AppServer")
+    dest_dir = f'C:\\TOTVS\\Protheus\\bin\\AppServer'
     dest_file = os.path.join(dest_dir, "appserver.ini")
 
     # Criar diretório de destino se não existir
@@ -27,8 +28,8 @@ def copiar_appserver_ini(version, log_box):
     # Copiar o arquivo
     try:
         shutil.copy(ini_file, dest_file)
-        log_box.insert(tk.END, f"Arquivo appserver.ini copiado para {dest_file}\n")
+        log_box.insert(tk.END, f"Arquivo appserver.ini copiado para {dest_file}.\n")
     except Exception as e:
-        log_box.insert(tk.END, f"Erro ao copiar o arquivo: {e}\n")
+        log_box.insert(tk.END, f"Erro ao copiar o arquivo: {str(e)}.\n")
 
     log_box.see(tk.END)
